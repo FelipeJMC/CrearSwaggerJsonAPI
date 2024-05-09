30772b6841f6debc24b84a30c5ab342a02ed3b6d
Felipe Morales <felipe.moralescandia@gmail.com>
2024-05-09 09:24:57 -0400

    Primer commit

---
+++ CrearSwaggerJSON/Controllers/SwaggerController.cs
@@ -0,0 +1,32 @@
+﻿using CrearSwaggerJSON.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrearSwaggerJSON.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwaggerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GenerateSwagger([FromBody] SwaggeParametrosModel parametros)
+        {
+            var swaggerDoc = new
+            {
+                openapi = "3.0.0", //aca puedes pasar la version como parametro tambien o dejarla fija..
+                info = new
+                {
+                    title = parametros.Title,
+                    description = parametros.Description,
+                    version = parametros.Version
+                },
+                paths = parametros.Paths.ToDictionary(
+                    path => path.Key,
+                    path => new { get = new { summary = path.Value.Summary, operationId = path.Value.OperationId, tags = path.Value.Tags } }
+                )
+            };
+
+            return Ok(swaggerDoc);
+        }
+    }
+}
---
+++ CrearSwaggerJSON/Models/SwaggeParametrosModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.XPath;
+
+namespace CrearSwaggerJSON.Models
+{
+    public class SwaggeParametrosModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public Dictionary<string, PathItem> Paths { get; set; }
+    }
+    public class PathItem
+    {
+        public string Summary { get; set; }
+        public string OperationId { get; set; }
+        public string[] Tags { get; set; }
+        // aca se pueden agregar mas parametros si necesitas para powerapps
+    }
+}
